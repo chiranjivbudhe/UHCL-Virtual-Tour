799a2d8c8135c449c8fd1938526b893014308b7a
chiranjiv budhe <budhe.chiranjiv@gmail.com>
2018-05-09 11:31:05 -0500

    Initial project commit

---
+++ vinsane/Assets/Scripts/controller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class controller : MonoBehaviour {
+
+    public GameObject[] PointsOfInterest;
+	public GameObject camera;
+    public int startingPoint = 0;
+    public Text continueText;
+    public RawImage rawImage;
+
+    private int currentPoint;
+
+	// Use this for initialization
+	void Start () {
+        currentPoint = startingPoint;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!PointsOfInterest [currentPoint].GetComponent<VideoPlayer> ().isPlaying) {
+			PointsOfInterest [currentPoint].transform.GetChild (0).GetChild (0).gameObject.SetActive(true);
+		}        
+	}
+
+	public void NextPointofInterest(){
+		if (!PointsOfInterest[currentPoint].GetComponent<VideoPlayer>().isPlaying)
+		{ 
+				if (currentPoint == (PointsOfInterest.Length - 1))
+				{
+					currentPoint = startingPoint;
+					continueText.enabled = true;
+					rawImage.enabled = false;
+				}
+				else
+				{
+				PointsOfInterest [currentPoint].transform.GetChild (0).GetChild (0).gameObject.SetActive(false);
+					currentPoint++;
+					RenderTexture renderTexture = new RenderTexture(256,256,24);
+					continueText.enabled = false;
+					rawImage.enabled = true;
+					PointsOfInterest[currentPoint].GetComponent<VideoPlayer>().targetTexture = renderTexture;
+					rawImage.texture = renderTexture;
+				}
+
+				camera.transform.position = PointsOfInterest[currentPoint].transform.position;
+				PointsOfInterest[currentPoint].GetComponent<VideoPlayer>().Play();
+			}
+	 }
+}
---
+++ vinsane/Assets/TimeGaze.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TimeGaze : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
+
+	Button button;
+	bool isEntered;
+	float timeElapsed = 0f;
+	float gazeActivationTime = 3f;
+	// Use this for initialization
+	void Start () {
+		button = GetComponent<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isEntered) {
+			timeElapsed += Time.deltaTime;
+			if (timeElapsed >= gazeActivationTime) {
+				button.onClick.Invoke ();
+				timeElapsed = 0f;
+				isEntered = false;
+			}
+		}
+	}
+
+	public void OnPointerEnter(PointerEventData eventdata){
+		isEntered = true;
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+		isEntered = false;
+		timeElapsed = 0f;
+	}
+
+
+}
